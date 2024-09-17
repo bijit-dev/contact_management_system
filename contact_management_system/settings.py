88a160a4bcb3e2c04a91a95f785963bd8b075e70
BIJIT DEB <bijitdeb0703@gmail.com>
2024-09-17 23:32:45 +0600

    admin add

--- contact_management_system/settings.py
+++ contact_management_system/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contacts.apps.ContactsConfig',
-    'from',
-    'todo',
+    
 ]
 
 MIDDLEWARE = [
